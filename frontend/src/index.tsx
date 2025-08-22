9ec8dea662d797454f1321a3ca889fad976c6313
Ch32hireC4t <csomoeli@students.zhaw.ch>
2025-08-23 00:23:20 +0200

    initial commit for frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,33 +1,18 @@
 import { render } from 'preact';
 
-import preactLogo from './assets/preact.svg';
+import { Link, Route, Router, Switch } from "wouter";
+import { Startpage } from './pages/startpage';
 import './style.css';
+import { Store } from './pages/store';
 
 export function App() {
 	return (
-		<div>
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
-			</section>
-		</div>
+		<Router>
+			<Switch>
+				<Route path='/store' component={Store}/>
+				<Route component={Startpage}/>
+			</Switch>
+		</Router>
 	);
 }
 
---
+++ frontend/src/pages/store.tsx
@@ -0,0 +1,38 @@
+export function Store(){
+    return <div>
+        <a href="./" class="button"><button>Home</button></a>
+
+        StorePage
+        <svg width="600" height="600" xmlns="http://www.w3.org/2000/svg">
+
+
+
+
+          <g id="gestell1" fill="red" stroke-width="5" transform="translate(40,16)">
+            <desc>Oberer Teil des Gestells </desc>
+            
+              <rect id="00" width="27.5" height="20" x="0" y="0" stroke="black" fill="transparent" />
+              <rect id="01" width="27.5" height="20" x="27.5" y="0" stroke="black" fill="transparent" />
+            <desc> Linker Teil des Gestells </desc>
+              <rect id="10" width="20" height="80" x="0" y="20" stroke="black" fill="transparent" />
+              <rect id="11" width="20" height="80" x="0" y="100" stroke="black" fill="transparent" />
+              <rect id="12" width="20" height="80" x="0" y="180" stroke="black" fill="transparent" />
+              <rect id="13" width="20" height="80" x="0" y="260" stroke="black" fill="transparent" />
+              <rect id="14" width="20" height="80" x="0" y="340" stroke="black" fill="transparent" />
+
+            <desc> Rechter Teil des Gestells </desc>
+              <rect id="20" width="20" height="80" x="35" y="20" stroke="black" fill="transparent" />
+              <rect id="21" width="20" height="80" x="35" y="100" stroke="black" fill="transparent" />
+              <rect id="22" width="20" height="80" x="35" y="180" stroke="black" fill="transparent" />
+              <rect id="23" width="20" height="80" x="35" y="260" stroke="black" fill="transparent" />
+              <rect id="24" width="20" height="80" x="35" y="340" stroke="black" fill="transparent" />
+
+            <desc> Unterer Teil des Gestells </desc>
+              <rect id="30" width="27.5" height="20" x="0" y="420" stroke="black" fill="transparent" />
+              <rect id="31" width="27.5" height="20" x="27.5" y="420" stroke="black" fill="transparent" />
+          </g>
+
+
+        </svg>
+    </div>
+}
