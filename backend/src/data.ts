6c3ffbd48e4e449284c8d45069eeeff9ea8c5eb3
UndyingWraith <undyingwraith@outlook.com>
2025-08-23 00:12:40 +0200

    basic backend functions

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,20 +1,29 @@
-import { IProduct, ICategory } from 'interfaces';
+import type { IServerCategory, IServerLocation, IServerProduct } from './extra';
 
-export const categories: ICategory[] = [
+export const categories: IServerCategory[] = [
 	{
+		id: '42fceff7-deb1-41ab-a4a0-ccd35ad8fd409',
 		name: 'Getränke',
 	},
 	{
+		id: 'a8aa090a-b5d4-4359-9aa1-b45e01ecb809',
 		name: 'Bier',
-		parent: {
-			name: 'Getränke',
-		}
+		parentId: '42fceff7-deb1-41ab-a4a0-ccd35ad8fd40'
 	}
 ];
 
-export const products: IProduct[] = [
+export const products: IServerProduct[] = [
 	{
+		id: 'ebf18433-2fbd-4856-86cf-ac9604062b0a',
 		name: 'Noser Bier',
-		category: categories[1],
+		categoryId: 'a8aa090a-b5d4-4359-9aa1-b45e01ecb809',
+		locationId: '9194a6fd-7492-419b-9708-14be317172e0',
+	}
+];
+
+export const locations: IServerLocation[] = [
+	{
+		id: '9194a6fd-7492-419b-9708-14be317172e0',
+		name: 'Kühlschrank 1'
 	}
 ];
--- interfaces/src/IProduct.ts
+++ interfaces/src/IProduct.ts
@@ -2,6 +2,7 @@ import { ICategory } from './ICategory';
 import { ILocation } from './ILocation';
 
 export interface IProduct {
+	id: string;
 	name: string;
 	location: ILocation;
 	category: ICategory;
