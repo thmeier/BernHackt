debe69272008fe768b06fc73da21130df340fa0f
Ch32hireC4t <csomoeli@students.zhaw.ch>
2025-08-23 10:27:40 +0200

    Beautify Start Button

--- frontend/src/pages/startpage.tsx
+++ frontend/src/pages/startpage.tsx
@@ -2,9 +2,13 @@ import { Store } from './store';
 import { Link, Route, Switch } from "wouter";
 
 export function Startpage(){
-    return <div style=" display: flex; justify-content: center;align-items: center; height: 100vh;">
-        Startpage
-        <a href="./store" class="button"><button>Start</button></a>
-    </div>
+    return <div>
+            <h1 style="text-align: center;">Startpage</h1>
+            <div style=" display: flex; justify-content: center;align-items: center; height: 100vh;">
+        
+            <a href="./store" class="button"><button style="padding-left: 30px; padding-right: 30px"><h2>Start</h2></button></a>
+        </div>
+
+        </div>
 
 }
