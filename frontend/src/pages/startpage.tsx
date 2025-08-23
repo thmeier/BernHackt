32ccb9a098bc47ec19965c089611840fd1857392
Ch32hireC4t <csomoeli@students.zhaw.ch>
2025-08-23 10:21:22 +0200

    Center elements in start page

--- frontend/src/pages/startpage.tsx
+++ frontend/src/pages/startpage.tsx
@@ -2,7 +2,7 @@ import { Store } from './store';
 import { Link, Route, Switch } from "wouter";
 
 export function Startpage(){
-    return <div>
+    return <div style=" display: flex; justify-content: center;align-items: center; height: 100vh;">
         Startpage
         <a href="./store" class="button"><button>Start</button></a>
     </div>
